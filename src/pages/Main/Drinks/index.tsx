b35bf35de73d098c38fbdee9ac8955a6eabd45e1
Victor Melo <melovictor515@gmail.com>
2023-10-13 21:21:44 -0300

    implementação json-server e criação das requests

--- src/pages/Main/Drinks/index.tsx
+++ src/pages/Main/Drinks/index.tsx
@@ -1,43 +1,28 @@
+import { useState, useEffect} from "react"
+
 import { Head } from "../../../components/Head"
 import { SnackTitle } from "../../../components/SnackTitle"
 import { Snacks } from "../../../components/Snacks"
 
+import { getDrinks } from "../../../services/api"
+
 export default function Drinks() {
 
-  const data = [
-    {
-      id: 1,
-      snack: 'drink',
-      name: 'Coca-Cola Lata',
-      description: 'O clássico refresco efervescente.',
-      price: 7.0,
-      image: 'https://res.cloudinary.com/vuca-solution/image/upload/w_800,c_fill,q_auto,fl_lossy/v1678206520/storage.vucasolution.com.br/lifeboxburger/arqs/produtos/aswtbwxsl8mxbfotb9y6.jpg'
-    },
-    {
-      id: 2,
-      snack: 'drink',
-      name: 'Guaraná Lata',
-      description: 'A refrescância e vitalidade do Brasil em uma garrafa.',
-      price: 6.5,
-      image: 'https://i.ytimg.com/vi/manvTmwKgvg/hq720.jpg?sqp=-oaymwE7CK4FEIIDSFryq4qpAy0IARUAAAAAGAElAADIQj0AgKJD8AEB-AH-CYAC0AWKAgwIABABGC4gZSgxMA8=&rs=AOn4CLDAwZU5-0iVqM-b2eTUV2cD0-mKOw'
-    },
-    {
-      id: 3,
-      snack: 'drink',
-      name: 'Suco de Laranja',
-      description: 'Puro sabor cítrico da natureza.',
-      price: 5.5,
-      image: 'https://i.pinimg.com/474x/39/46/89/3946894a2290cb6a74ebd8b1ff186421.jpg'
-    },
-  ]
+  const [drinks, setDrinks] = useState([])
 
-  console.log('data', data)
+  useEffect(() => {
+    ;(async () => {
+      const drinkRequest = await getDrinks()
+
+      setDrinks(drinkRequest.data)
+    })()
+  },[])
 
   return(
     <>
       <Head title="Bebidas"/>
       <SnackTitle>Bebidas</SnackTitle>
-      <Snacks snacks={data}></Snacks>
+      <Snacks snacks={drinks}></Snacks>
     </>
   )
 }
--- src/pages/Main/Pizzas/index.tsx
+++ src/pages/Main/Pizzas/index.tsx
@@ -1,43 +1,27 @@
+import { useState, useEffect } from "react"
+
 import { Head } from "../../../components/Head"
 import { SnackTitle } from "../../../components/SnackTitle"
 import { Snacks } from "../../../components/Snacks"
+import { getPizzas } from "../../../services/api"
 
 export default function Pizzas() {
 
-  const data = [
-    {
-      id: 1,
-      snack: 'pizza',
-      name: 'Costela com barbecue',
-      description: 'A fusão perfeita entre carne suculenta e molho barbecue, em cada fatia. Deliciosamente defumada!',
-      price: 60.5,
-      image: 'https://blog.novasafra.com.br/wp-content/uploads/2018/05/ContainerXdaXPizza.jpg'
-    },
-    {
-      id: 2,
-      snack: 'pizza',
-      name: 'Camarão com catupiry',
-      description: ' A combinação irresistível de camarões suculentos com o cremoso sabor do catupiry. Uma explosão de sabores marinhos!',
-      price: 65.5,
-      image: 'https://vivariomarrecife.com.br/wp-content/uploads/2019/07/Atl%C3%A2ntico-Camar%C3%A3o-com-Catupiri-Cortada.jpg'
-    },
-    {
-      id: 3,
-      snacl: 'pizza',
-      name: 'Calabresa acebolada',
-      description: 'O toque picante da calabresa combinado com a doçura das cebolas caramelizadas. Um casamento perfeito de sabores.',
-      price: 50.5,
-      image: 'https://fyoti.com.br/wp-content/uploads/2020/01/Receita-de-Pizza-de-liquidificador-Diferente-Pode-Adicionar-Calabresa.jpg'
-    },
-  ]
+  const [Pizzas, setPizzas] = useState([])
 
-  console.log('data', data)
+  useEffect(() => {
+    ;(async () => {
+      const pizzasRequest = await getPizzas()
+
+      setPizzas(pizzasRequest.data)
+    })()
+  },[])
 
   return(
     <>
       <Head title="Pizzas"/>
       <SnackTitle>Pizzas</SnackTitle>
-      <Snacks snacks={data}></Snacks>
+      <Snacks snacks={Pizzas}></Snacks>
     </>
   )
 }
